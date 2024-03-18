053fdc51e9598e91e350348defebf387ff185403
Andre Mauldin <cam94@att.net>
2024-03-17 21:23:31 -0500

    adding model

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 import os
 import json
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+# import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
 
 from flask import (Flask, redirect, render_template, request, send_from_directory, url_for)
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
-
 
 @app.route('/')
 def index():
@@ -29,23 +35,54 @@ def hello():
        print('Request for hello page received with no name or blank name -- redirecting')
        return redirect(url_for('index'))
 
+try:
+    USAhousing = pd.read_csv('USA_Housing.csv')
+    X = USAhousing[['Avg. Area Income', 'Avg. Area House Age', 'Avg. Area Number of Rooms',
+                'Avg. Area Number of Bedrooms', 'Area Population']]
+    y = USAhousing['Price']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=101)
+    lm = LinearRegression()
+    fit = lm.fit(X_train,y_train)
+    coeff_df = pd.DataFrame(lm.coef_,X.columns,columns=['coeff'])
+    predictions = lm.predict(X_test)
+except Exception as e:
+    print(e)
+
 @app.route('/getDetails', methods=['POST'])
 def getDetails():
-    request_data = request.get_json()
-    print(request_data['data']['key'])
-    if (request_data['data']['key'] == 'hello'):
-        details = {
-            "flowStatus": "SUCCESS",
-            "flowStatusMessage": "",
-            "result": {
-                "data": "Backend Works!"
+    try:
+        request_data = request.get_json()
+        print(request_data['data']['data'])
+        if (request_data['data']['key'] == os.environ.get('KEY')):
+            housingInput = pd.DataFrame(request_data['data']['data'])
+            print(housingInput)
+            pricePrediction = lm.predict(housingInput)
+            details = {
+                "flowStatus": "SUCCESS",
+                "flowStatusMessage": "",
+                "result": {
+                    "intercept": lm.intercept_,
+                    "coefficients": coeff_df.to_json(),
+                    "prediction": pricePrediction[0],
+                    "metrics": {
+                        'mse': metrics.mean_squared_error(y_test, predictions),
+                        'mae': metrics.mean_absolute_error(y_test, predictions),
+                        'rmse': np.sqrt(metrics.mean_squared_error(y_test, predictions))
+                    }
+                }
             }
-        }
-        return details
-    else:
+            str(details)
+            return details
+        else:
+            return {
+                "flowStatus": "FAILURE",
+                "flowStatusMessage": "Backend Error!",
+                "result": {}
+            }
+    except Exception as e:
         return {
             "flowStatus": "FAILURE",
-            "flowStatusMessage": "Backend Error!",
+            "flowStatusMessage": str(e),
             "result": {}
         }
 
